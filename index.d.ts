afbcd37e0413bbb8d9e24f70114a28ce33c3403c
Baruch Velez <baruch.velez@mcmakler.de>
2019-04-10 11:31:23 +0200

    .

--- index.d.ts
+++ index.d.ts
@@ -1,42 +1,43 @@
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+
 declare module 'resq' {
-    interface RESQTreeNode {
+    interface ReactSelectorQueryTreeNode {
         name: string | undefined
-        children: Array<RESQTreeNode>
+        children: Array<ReactSelectorQueryTreeNode>
         props: any
         state: any
         node: HTMLElement
     }
 
-    function resq$(
-        selector: string,
-        options: { timeout: string, rootElSelector: string }
-    ): Promise<RESQ>
-
-    function resq$$ (
-        selector: string,
-        options: { timeout: string, rootElSelector: string }
-    ): Promise<RESQ>
-
-    class RESQNodes extends Array {
-        constructor(nodes: Array<RESQTreeNode>)
-        public byProps<P extends {}>(props: P): RESQNodes
-        public byState<S extends {}>(props: S): RESQNodes
+    class ReactSelectorQueryNodes extends Array {
+        constructor(nodes: Array<ReactSelectorQueryTreeNode>)
+        public byProps<P extends Object>(props: P): ReactSelectorQueryNodes
+        public byState<S extends Object>(props: S): ReactSelectorQueryNodes
     }
 
-    class RESQNode extends Object {
-        constructor(item: RESQTreeNode, nodes: Array<RESQTreeNode>)
-        public byProps<P extends {}>(props: P): RESQNode
-        public byState<S extends {}>(props: S): RESQNode
+    class ReactSelectorQueryNode extends Object {
+        constructor(item: ReactSelectorQueryTreeNode, nodes: Array<ReactSelectorQueryTreeNode>)
+        public byProps<P extends Object>(props: P): ReactSelectorQueryNode
+        public byState<S extends Object>(props: S): ReactSelectorQueryNode
     }
 
-    class RESQ {
+    class ReactSelectorQuery {
         private selectors: Array<string>
         private rootComponent: HTMLElement
-        private tree: RESQTreeNode
-        private nodes?: Array<RESQTreeNode>
+        private tree: ReactSelectorQueryTreeNode
+        private nodes?: Array<ReactSelectorQueryTreeNode>
 
         constructor(selector: string, root: HTMLElement)
-        public find: () => RESQNode
-        public findAll: () => RESQNodes
+        public find: () => ReactSelectorQueryNode
+        public findAll: () => ReactSelectorQueryNodes
     }
+
+    function resq$(selector: string): Promise<ReactSelectorQuery>
+    function resq$$(selector: string): Promise<ReactSelectorQuery>
+    function getElementType(element: ReactSelectorQueryTreeNode): string;
+    function verifyIfArgs<A1 extends Array<any>, A2 extends Array<any>>(arr1: A1, arr2: A2): boolean;
+    function match<M extends Object, V extends Object>(matcher: M, verify: V): boolean;
+    function removeChildrenFromProps<P extends { children: any }>(props: P): string | Omit<P, 'children'>;
+    function getElementState<S>(elementState: S): {} | S;
+    function buildNodeTree(element: Object): ReactSelectorQueryTreeNode;
 }
