39dc08cf986148b21b47038a83ea40b118e14a31
Jay Chen <jay.chen@hbo.com>
2019-05-08 17:56:17 +0800

    query from non-root element

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import ReactSelectorQuery from './src/resq'
 import { waitToLoadReact } from './src/waitToLoadReact'
 
-function doQuery(selector, method = 'find') {
+function doQuery(selector, method = 'find', reactElement) {
     if (!global.isReactLoaded) {
         throw new Error('Could not find the root element of your application')
     }
 
-    return new ReactSelectorQuery(selector, global.rootReactElement)[method]()
+    return new ReactSelectorQuery(selector, reactElement || global.rootReactElement)[method]()
 }
 
-export function resq$(selector) {
-    return doQuery(selector)
+export function resq$(selector, reactElement) {
+    return doQuery(selector, 'find', reactElement)
 }
 
-export function resq$$(selector) {
-    return doQuery(selector, 'findAll')
+export function resq$$(selector, reactElement) {
+    return doQuery(selector, 'findAll', reactElement)
 }
 
 export { waitToLoadReact }
