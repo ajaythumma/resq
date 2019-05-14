274705ee194dad236f3df7e01a7a1ffb48e15bf0
Jay Chen <jay.chen@hbo.com>
2019-05-14 15:54:39 +0800

    find react from dom

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ function doQuery(selector, method = 'find', reactElement) {
         throw new Error('Could not find the root element of your application')
     }
 
-    return new ReactSelectorQuery(selector, reactElement || global.rootReactElement)[method]()
+    return new ReactSelectorQuery(selector, (reactElement && findReactComponent(reactElement)) || global.rootReactElement)[method]()
+}
+
+function findReactComponent(domNode) {
+    const key = Object.keys(domNode).find(key => key.startsWith('__reactInternalInstance'))
+    if (key) {
+	    return domNode[key]
+    } else {
+	    throw new Error('No react component for this element')
+    }
 }
 
 export function resq$(selector, reactElement) {
